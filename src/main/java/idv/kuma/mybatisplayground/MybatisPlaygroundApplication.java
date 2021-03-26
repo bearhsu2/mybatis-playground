4a630f193b3b78419ce2eb1e934d9661ce3e96f9
Yu-Song Syu <kuma@simbo.com.tw>
2021-03-26 10:39:23 +0800

    init

---
+++ src/main/java/idv/kuma/mybatisplayground/MybatisPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package idv.kuma.mybatisplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisPlaygroundApplication.class, args);
+	}
+
+}
